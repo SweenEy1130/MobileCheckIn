a4528061041336a18864bed4fb16bef4a91b5ec0
SweenEy1130 <ronnie.alonso@gmail.com>
2013-07-11 15:16:25 +0800

    fix face.py bug

--- face.py
+++ face.py
@@ -1,4 +1,5 @@
 # coding=gbk
+
 # $File: face.py
 # $Date: Sun Feb 24 14:47:27 2013 +0800
 # $Author: ronnie.alonso@gmail.com
@@ -36,14 +37,14 @@ example:	HTTP Request HEADER
 class FaceppHandler(BaseHandler):
 	def handle_request(self,response):
 		if response.error:
-			print 'error:1;info:'+response.error
+			# print 'error:1;info:'+response.error
 			self.write({'error':1,'info':response.error})
 			self.finish()
 			return
 		else:
 			face_detect = json.loads(response.body)
-			if not face_detect['face']:
-				print 'error:3;info:NO FACE'
+			if not face_detect.has_key('face'):
+				# print 'error:3;info:NO FACE'
 				self.write({'error':3 , 'info':'NO FACE'})
 				self.finish()
 				return
@@ -65,19 +66,19 @@ class FaceppHandler(BaseHandler):
 			print 'error:0;	similarity:	%f' % similarity			
 			self.write({"error":0,"similarity":similarity})
 		else:
-			print 'error:3;info:no similarity'
+			# print 'error:3;info:no similarity'
 			self.write({"error":1})
 		self.finish() 
 
 	@tornado.web.asynchronous
 	def post(self):
 		if not self.current_user:
-			print 'error:2;info:not login'
+			# print 'error:2;info:not login'
 			self.write({"error":2})
 			self.finish()
 			return
 		if (not self.get_secure_cookie("sessionid")):
-			print 'error:4;info:not create'
+			# print 'error:4;info:not create'
 			self.write({"error":4})
 			self.finish()
 			return
@@ -125,14 +126,14 @@ example:	HTTP Request HEADER
 class FaceRegisterHandler(BaseHandler):
 	def handle_request(self,response):
 		if response.error:
-			print 'error:1;info:'+response.error
+			# print 'error:1;info:'+response.error
 			self.write({'error':1,'info':response.error})
 			self.finish()
 			return
 		else:
 			face_detect = json.loads(response.body)
-			if not face_detect['face']:
-				print 'error:5;info:No face is detected'
+			if not face_detect.has_key('face'):
+				# print 'error:5;info:No face is detected'
 				self.write({'error':5,'info':'No face is detected'})
 				self.finish()
 				return
@@ -145,11 +146,16 @@ class FaceRegisterHandler(BaseHandler):
 
 	def handle_request2(self,response):
 		add_face=json.loads(response.body)
-		if add_face["success"] == True:
+		if (not add_face.has_key("success")):
+			self.write({"error":3})
+
+		elif (add_face["success"] == True):
 			self.db.execute("UPDATE USER SET IMAGESAMPLE = \'%s\' WHERE UID = %s" % (self.tmp_face1 , self.uid))
 			self.write({"error":0,"faceid":self.tmp_face1})
+
 		else:
 			self.write({"error":3})
+			
 		self.finish()
 
 	@tornado.web.asynchronous
