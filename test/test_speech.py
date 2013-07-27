9d2c6c7a44fb1a9c70e10b58ce6e44c7a5aeba22
SweenEy1130 <ronnie.alonso@gmail.com>
2013-07-27 18:48:59 +0800

    speech test

--- test/test_speech.py
+++ test/test_speech.py
@@ -1,3 +1,10 @@
+# coding=gbk
+# $File: test_speech.py
+# $Date: Sat Jul 27 18:15:27 2013 +0800
+# $Author: ronnie.alonso@gmail.com
+#
+
+import os , json , glob , string , sys
 from ctypes import cdll, c_int , c_char_p , c_double
 
 # Speech Verify Engine initialize
@@ -8,7 +15,7 @@ sv_dll.SVdetect.restype = c_double
 
 def SVtrain(uid , file1 , file2 , file3):
 	try:
-		ret = sv_dll.SVtrain3("../sv/sv.0.0.3.2.bin" , "../static/audio_mod/%s.bin" % (uid) , file1 , file2 , file3)
+		ret = sv_dll.SVtrain3("../../sv/sv.0.0.3.2.bin" , "../audio_mod/%s.bin" % (uid) , file1 , file2 , file3)
 		# print ret
 		if (ret == 1):
 			return "error:1;info:newEngine error"
@@ -22,15 +29,38 @@ def SVtrain(uid , file1 , file2 , file3):
 def SVdetect(uid , filename):
 
 	try:
-		ret = sv_dll.SVdetect("../sv/sv.0.0.3.2.bin" , "../static/audio_mod/%d.bin" % (uid) , filename , 0.7 ,1)
+		ret = sv_dll.SVdetect("../../sv/sv.0.0.3.2.bin" , "../audio_mod/%s.bin" % (uid) , filename , 0.7 ,1)
 		return ret
 	except:
 		return "error:-1;info:failure in detect"
 
 if __name__ == '__main__':
 
-	ret = SVtrain(5101109071 , "../static/audio/5110309747161534321583.wav" , "../static/audio/5110309747161534321272.wav" , "../static/audio/5110309747161534320824.wav")
-	print ret
+	userlist = []
 
-	ret = SVdetect(5101109071 , "audio/2.wav")
-	print ret
+	os.chdir("identical")
+
+	
+	for filename in glob.glob("*.wav"):
+		uid = filename[0:5]
+		print userlist
+		if (uid not in userlist):
+			userlist.append(uid)
+			output = open("../output/%s" % uid , "w")
+			output.write("%s\nFilename\tSimilarity\n" % uid)
+
+			filepath = filename
+			ret = SVtrain(uid , filepath , filepath , filepath)
+
+			if(ret == 0):
+				for cmpfile in glob.glob("*.wav"):
+					cmp_id = cmpfile[0:5]
+
+					cmp_path = cmpfile
+					sim = SVdetect(uid , cmp_path)
+
+					output.write("%s\t%s\n" % (cmp_id , sim))
+			else:
+				print "Train Error!\n"
+
+			output.close()
